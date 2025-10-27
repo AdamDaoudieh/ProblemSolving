4ec381396f3094d03edd9ce6776c8e759261df6f
Adam-quiqflow <adam@quiqflow.com>
2025-10-27 22:36:46 +0300

    allcomp done

--- numofcomp.js
+++ numofcomp.js
@@ -1,1 +1,35 @@
 
+
+function counts(str) {
+    const dict = {};
+    for (let char of str) {
+        dict[char] = (dict[char] || 0) + 1;
+    }
+    return dict;
+}
+
+
+function allpossible(str) {
+    const result = [];
+
+    
+    function backtrack(current, charCount) {
+        if (current.length === str.length) {
+            result.push(current);
+            return;
+        }
+        
+        for (let char in charCount) {
+            if (charCount[char] > 0) {
+                charCount[char]--;
+                backtrack(current + char, charCount);
+                charCount[char]++;
+            }
+        }
+    }
+    
+    backtrack('', counts(str));
+    return result;
+}
+
+console.log(allpossible('abc'));
